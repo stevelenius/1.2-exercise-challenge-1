0728c6172c6d8e12357bd144f942bdb90333e4da
stevelenius <44826562+stevelenius@users.noreply.github.com>
2020-01-26 17:48:36 -0600

    initial commit
    before code cleanup

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800,600);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+  	fill(255);
+  	ellipse(mouseX,mouseY, 80, 80);
+  } else {
+  	fill(0);
+  }
+  
+}
